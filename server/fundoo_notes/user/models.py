b831a205f771962dd2f7bfd801b25aa39183559d
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-08-19 20:47:13 +0530

    [Aniket Wagh]feat: extend AbstractUser to include custom fields

--- server/fundoo_notes/user/models.py
+++ server/fundoo_notes/user/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class User(AbstractUser):
+    is_verified = models.BooleanField(default=False)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
