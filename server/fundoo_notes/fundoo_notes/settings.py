f2157ccaee5525bf2fbc70f5b94144c4c37044da
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-08-21 14:49:20 +0530

    [Aniket Wagh]Refactor: Refactor setting.py for use of simplejwt

--- server/fundoo_notes/fundoo_notes/settings.py
+++ server/fundoo_notes/fundoo_notes/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -107,6 +108,18 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# JWT settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+}
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
